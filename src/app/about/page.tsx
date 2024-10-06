cde56f84683190855e5f04700c3b94389fd46f66
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2024-10-06 17:26:56 -0300

    udapte

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+function AboutPage() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-2xl font-bold my-4">About Page</h1>
+      <p className="text-gray-300">This is the about page content.</p>
+    </div>
+  );
+}
+export default AboutPage;
